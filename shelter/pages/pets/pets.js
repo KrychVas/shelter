009d4c09e2cfcb96d3247929c48716b49cb6a38b
Vasyl Krychfalushii <krychvas03@gmail.com>
2026-06-30 22:28:55 +0200

    feat: use hybrid data with official texts and correct image paths

--- shelter/pages/pets/pets.js
+++ shelter/pages/pets/pets.js
@@ -1,11 +1,11 @@
 document.addEventListener('DOMContentLoaded', () => {
   const body = document.body;
 
-  // Твій новий оригінальний JSON-масив
+  // Офіційні тексти + твої перевірені робочі назви файлів картинок
   const petsData = [
     {
       "name": "Jennifer",
-      "img": "../../assets/images/jennifer.png",
+      "img": "../../assets/image/pets-jennifer.png",
       "type": "Dog",
       "breed": "Labrador",
       "description": "Jennifer is a sweet 2 months old Labrador that is patiently waiting to find a new forever home. This girl really enjoys being able to go outside to run and play, but won't hesitate to play up a storm in the house if she has all of her favorite toys.",
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     {
       "name": "Sophia",
-      "img": "../../assets/images/sophia.png",
+      "img": "../../assets/image/pets-sophia.png",
       "type": "Dog",
       "breed": "Shih tzu",
       "description": "Sophia here and I'm looking for my forever home to live out the best years of my life. I am full of energy. Everyday I'm learning new things, like how to walk on a leash, go potty outside, bark and play with toys and I still need some practice.",
@@ -27,7 +27,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     {
       "name": "Woody",
-      "img": "../../assets/images/woody.png",
+      "img": "../../assets/image/pets-woody.png",
       "type": "Dog",
       "breed": "Golden Retriever",
       "description": "Woody is a handsome 3 1/2 year old boy. Woody does know basic commands and is a smart pup. Since he is on the stronger side, he will learn a lot from your training. Woody will be happier when he finds a new family that can spend a lot of time with him.",
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     {
       "name": "Scarlett",
-      "img": "../../assets/images/scarlett.png",
+      "img": "../../assets/image/pets-scarlet.png", // Твоя стара робоча назва
       "type": "Dog",
       "breed": "Jack Russell Terrier",
       "description": "Scarlett is a happy, playful girl who will make you laugh and smile. She forms a bond quickly and will make a loyal companion and a wonderful family dog or a good companion for a single individual too since she likes to hang out and be with her human.",
@@ -49,7 +49,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     {
       "name": "Katrine",
-      "img": "../../assets/images/katrine.png",
+      "img": "../../assets/image/pets-katrine.png",
       "type": "Cat",
       "breed": "British Shorthair",
       "description": "Katrine is a beautiful girl. She is as soft as the finest velvet with a thick lush fur. Will love you until the last breath she takes as long as you are the one. She is picky about her affection. She loves cuddles and to stretch into your hands for a deeper relaxations.",
@@ -60,7 +60,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     {
       "name": "Timmy",
-      "img": "../../assets/images/timmy.png",
+      "img": "../../assets/image/pets-timmy.png",
       "type": "Cat",
       "breed": "British Shorthair",
       "description": "Timmy is an adorable grey british shorthair male. He loves to play and snuggle. He is neutered and up to date on age appropriate vaccinations. He can be chatty and enjoys being held. Timmy has a lot to say and wants a person to share his thoughts with.",
@@ -71,7 +71,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     {
       "name": "Freddie",
-      "img": "../../assets/images/freddie.png",
+      "img": "../../assets/image/pets-freddie.png",
       "type": "Cat",
       "breed": "British Shorthair",
       "description": "Freddie is a little shy at first, but very sweet when he warms up. He likes playing with shoe strings and bottle caps. He is quick to learn the rhythms of his human’s daily life. Freddie has bounced around a lot in his life, and is looking to find his forever home.",
@@ -82,7 +82,7 @@ document.addEventListener('DOMContentLoaded', () => {
     },
     {
       "name": "Charly",
-      "img": "../../assets/images/charly.png",
+      "img": "../../assets/image/pets-charly.png",
       "type": "Dog",
       "breed": "Jack Russell Terrier",
       "description": "This cute boy, Charly, is three years old and he likes adults and kids. He isn’t fond of many other dogs, so he might do best in a single dog home. Charly has lots of energy, and loves to run and play. We think a fenced yard would make him very happy.",
@@ -92,8 +92,6 @@ document.addEventListener('DOMContentLoaded', () => {
       "parasites": ["lice", "fleas"]
     }
   ];
-
-  const imgPrefix = '../../assets/images/';
 
   /* ==========================================================================
      БЕРГЕР МЕНЮ ДЛЯ PETS
@@ -137,11 +135,10 @@ document.addEventListener('DOMContentLoaded', () => {
   }
 
   function openModal(pet) {
-    const imgName = pet.img.split('/').pop();
     popup.innerHTML = `
       <div class="popup__wrapper">
         <button class="button-round popup__close">✕</button>
-        <img class="popup__image" src="${imgPrefix + imgName}" alt="${pet.name}" />
+        <img class="popup__image" src="${pet.img}" alt="${pet.name}" />
         <div class="popup__content">
           <h3 class="popup__title">${pet.name}</h3>
           <h4 class="popup__subtitle">${pet.type} - ${pet.breed}</h4>
@@ -223,10 +220,9 @@ document.addEventListener('DOMContentLoaded', () => {
     pageItems.forEach(id => {
       const pet = petsData[id];
       if (!pet) return;
-      const imgName = pet.img.split('/').pop();
       gridContainer.insertAdjacentHTML('beforeend', `
         <div class="pet-card">
-          <img src="${imgPrefix + imgName}" alt="${pet.name}" class="pet-card__img">
+          <img src="${pet.img}" alt="${pet.name}" class="pet-card__img">
           <h3 class="pet-card__name">${pet.name}</h3>
           <button class="button-secondary">Learn more</button>
         </div>
